ffd86feaadf90e94c44cdf0a6da0ef252e561986
In28 <ina.moisejeva@gmail.com>
2023-11-20 15:57:18 +0200

    array

---
+++ script.js
@@ -0,0 +1,24 @@
+//1
+
+var names = ["Jon Snow", "Daenerys Targaryen", "Theon Greyjoy", "Tyrion Lannister", "Arya Stark"];
+
+console.log("Array:", names);
+console.log("Pirmas:", names[0]);
+console.log("Paskutinis:", names[names.length - 1]);
+
+var number = prompt("Įveskite skaičių nuo 1 iki 5");
+if (!isNaN(number) && number >= 1 && number <= 5) {
+  console.log("Personažas:", names[number - 1]);
+}
+
+
+//2
+
+var skaicius = prompt("Įveskite skaičių") 
+var suma = 0;
+var didziausias = number
+var maziausias = number
+
+
+
+
